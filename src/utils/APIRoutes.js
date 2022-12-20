f3daccc2037685807a3db46ac478e33656c3d1e1
Prathamesh dhasade <prathameshdhasade2015@gmail.com>
2022-12-20 18:12:34 +0530

    chat website

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+export const host = "http://localhost:5000";
+export const loginRoute = `${host}/auth/login`;
+export const registerRoute = `${host}/auth/register`;
+export const logoutRoute = `${host}/auth/logout`;
+export const allUsersRoute = `${host}/auth/allusers`;
+export const sendMessageRoute = `${host}/messages/addmsg`;
+export const recieveMessageRoute = `${host}/messages/getmsg`;
+export const setAvatarRoute = `${host}/auth/setavatar`;
